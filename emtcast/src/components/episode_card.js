b8e94c10993178fe8c7136a1202c40dc383f3dd9
Matt Hughes <matthew.l.hughes@kp.org>
2020-01-31 09:44:11 -0800

    aligning dot

--- emtcast/src/components/episode_card.js
+++ emtcast/src/components/episode_card.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import './styling/episode_card.css';
+import React, { Component } from "react";
+import "./styling/episode_card.css";
 
 export default class episode_card extends Component {
-	render() {
-		return (
-			<div className="container">
-				<div className="triangle">
-					<i class="fa fa-play fa-2x"></i>
-				</div>
-				<div className="grid-item">
-					<h4>{this.props.data.title}</h4>
-					<p>{this.props.data.time}</p>
-					<p>{this.props.data.description}</p>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container">
+        <div className="triangle">
+          <i className="fa fa-play fa-2x"></i>
+        </div>
+        <div className="grid-item">
+          <h4>{this.props.data.title}</h4>
+          <p>{this.props.data.time}</p>
+          <p>{this.props.data.description}</p>
+        </div>
+      </div>
+    );
+  }
 }
---
+++ emtcast/src/components/time_dot.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import "./styling/time_dot.css";
+
+export default class time_dot extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dotLeft: 0,
+      dotTop: 50
+    };
+    this.startTimer = this.startTimer.bind(this);
+  }
+
+  startTimer() {
+    this.setState({
+      dotLeft: this.state.dotLeft
+    });
+    this.timer = setInterval(
+      () =>
+        this.setState({
+          dotLeft: this.state.dotLeft + 1
+        }),
+      1000
+    );
+  }
+  render() {
+    // let dotLeft = this.props.dotLeft;
+    // let dotTop = this.props.dotTop;
+    let dotStyle = {
+      border: "10px solid white",
+      borderRadius: "100%",
+      position: "absolute",
+      left: `${this.state.dotLeft}%`,
+      top: `${this.state.dotTop}%`
+    };
+    console.log(this.state.dotLeft + ", " + this.state.dotTop);
+    // let myVar = setInterval(this.myTimer, 1000);
+
+    return (
+      <div className="bgContainer">
+        <div style={dotStyle}></div>
+        <svg className="svgContainer">
+          <line className="line1" x1="0" y1="50%" x2="40%" y2="50%" />
+          <line className="line2" x1="40%" y1="50%" x2="45%" y2="30%" />
+          <line className="line3" x1="45%" y1="30%" x2="55%" y2="70%" />
+          <line className="line4" x1="55%" y1="70%" x2="60%" y2="50%" />
+          <line className="line5" x1="60%" y1="50%" x2="100%" y2="50%" />
+          {/* <BgLine /> */}
+        </svg>
+      </div>
+      // <button onClick={this.startTimer}>start</button>
+    );
+  }
+}
